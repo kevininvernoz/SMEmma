1a36c86d316e2ee7fe1e9e6bc904c1c1c57249d9
kevininvernoz <kevininvernoz@gmail.com>
2017-10-21 21:48:42 -0300

    clock added

--- Plugins/dateTime/dateTime.js
+++ Plugins/dateTime/dateTime.js
@@ -1,3 +1,143 @@
+
+ 
+
+const months = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre',
+				'Noviembre','Diciembre'];
+
+const days =['Domingo','Lunes','Martes','Miércoles','Jueves','Viernes','Sábado'];
+
+/*milli seconds*/
+const fadeValue = 600;
+const separatorFadeValue = 3000;
+
+/* seconds */
+const updateInterval = 10;
+
+const interval = 1000;
+
+function format(i) {
+
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
+
+
+function getTime() {
+	
+	var now = new Date(),
+		h = now.getHours();
+		m = now.getMinutes();
+		s = now.getSeconds ();
+
+	h = format(h);
+	m = format (m);
+	s = format (s);
+
+	return {
+		hour : h,
+		minutes : m,
+		seconds : s
+	};
+}
+
+function getDate(){
+
+    var date = new Date(),
+        year = date.getFullYear();
+        month = date.getMonth(),
+        dayNb = date.getDate(),
+        dayWeek = date.getDay();
+
+    return {
+            day : days[dayWeek],
+            date : dayNb,
+            month : months[month],
+            year : year
+        };
+}
+
+function displayClock(){    
+    
+    var time = getTime();
+
+    var hourcache = $('#hour').text();
+    var minutescache = $('#minutes').text();
+
+    if (hourcache !== time.hour)
+    {
+    	$('#hour').text(time.hour);
+    	displayDate();   	
+    }
+    
+    if (minutescache !== time.minutes)
+    {
+    	$('#minutes').text(time.minutes); 
+    }
+
+
+    $('#seconds').text(time.seconds);
+
+
+
+
+    
+}
+
+function displayDate(){
+    var date = getDate();
+    var cacheDate = $('#day').text();
+
+    /* date changed */
+    if(cacheDate !== date.day){
+      
+      $('#day').fadeOut(fadeValue, function () {
+          $(this).text(date.day);
+          $(this).fadeIn(fadeValue);
+        });
+
+      $('#date').fadeOut(fadeValue, function () {
+          $(this).text(date.date + " de " + date.month + " de " + date.year);
+          $(this).fadeIn(fadeValue);
+        });        
+    }            
+}
+
+displayDate();
+setInterval (function() {displayClock()},interval);
+
+
+
+/*
+function pulseClockSeparator(){
+
+     $('#separator').fadeOut(separatorFadeValue, function () {
+          $(this).text(":");
+          $(this).fadeIn(separatorFadeValue);
+        }); 
+}
+
+
+displayClock();
+displayDate();
+pulseClockSeparator();
+
+/* update 
+setInterval(function(){   
+   pulseClockSeparator();   
+}, separatorFadeValue * 2);
+
+
+setInterval(function(){
+   displayClock();
+   displayDate();
+}, updateInterval * 1000);
+
+
+
+/*
  
 const months = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre',
 				'Noviembre','Diciembre'];
@@ -52,6 +192,9 @@ function getTime(){
 
 function displayClock() {
 	var time = getTime();
+	alert("Hello! I am an alert box!!");
+	//$('#clock').text(time.hour)
+
 
 }
 
@@ -61,36 +204,5 @@ function displayDate() {
 }
 
 
-/*
- function clock ()
- {
- 	var today = new Date();
- 	var hour = today.getHours();
- 	var minutes = today.getMinutes();
- 	var seconds = today.getSeconds();
- 	minutes = checktime (minutes);
- 	seconds = checktime (seconds);
- 	//document.getElementById('clock').innerHTML =
-  //  h + ":" + m + ":" + s;
-
-  var pClock = document.getElementById("clock");
-  pClock.textContent = h + ":" + m + ":" + s;
- }
-
- function checktime ()
- {
- 	if (i < 10) {
- 		i = "0" + i;
- 	}
- 	return i;
- }
-
- function updateDateTime()
- {
- 	var t= setInterval (clock , 500);
-
- }
-
- updateDateTime();
-
+displayClock();
 */
